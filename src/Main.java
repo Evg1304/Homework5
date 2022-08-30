e39a1aafa70fd451c86fa4496c1a5f53c1a20512
mishe <mishel1400@mail.ru>
2022-08-30 16:22:45 +0300

    Сделала ДЗ

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 //Задание 3
-//        int[] numbers = {1, 2, 3};
-//        for (int i = 2; i < numbers.length && i >= numbers[0] || i >= numbers[0] - 1; i--) {
-//            System.out.print(numbers[i]);
-//            if (1 <= i && i < numbers.length) {
-//                System.out.print(", ");
-//            }
-//        }
+        int[] numbers = {1, 2, 3};
+        for (int i = 2; i < numbers.length && i >= numbers[0] || i >= numbers[0] - 1; i--) {
+            System.out.print(numbers[i]);
+            if (1 <= i && i < numbers.length) {
+                System.out.print(", ");
+            }
+        }
         //Задание 1
         int [] arrays = new int [3];
         arrays[0]=1;
@@ -26,11 +26,22 @@ public class Main {
             }
 
         }
+        for (int i = 0; i < arrays1.length; i++) {
+            System.out.println(arrays1[i]);
+            if (i!= arrays.length -1 ) {
+                System.out.print(",");
+            }
+        }for (int i = 0; i < arrays2.length; i++) {
+            System.out.println(arrays2[i]);
+            if (i!= arrays2.length -1 ) {
+                System.out.print(",");
+            }
+        }
         System.out.println();
         // Задание 4
         for (int i = 0;i<arrays.length;i++){
             if (arrays[i]%2 == 1){
-              arrays[i]++;
+                arrays[i]++;
             }
             System.out.print(arrays[i]);
         }
@@ -38,11 +49,4 @@ public class Main {
 
     }
 }
-
-
-
-
-
-
-
 
